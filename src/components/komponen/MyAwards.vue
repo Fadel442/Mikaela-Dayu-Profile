f6c9644407d9267777fad3b951c726f329cf3db9
Fadel442 <fadelmuhammad4402@gmail.com>
2024-10-28 19:14:42 +0700

    carousel vue

--- src/components/komponen/MyAwards.vue
+++ src/components/komponen/MyAwards.vue
@@ -1,5 +1,79 @@
+<script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel';
+
+
+export default {
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+    data() {
+        return {
+            config: {
+                autoplay: 2000,
+                // wrapAround: true,
+                // loop: true,
+                pauseAutoplayOnHover: true,
+            },
+            images: [
+                {
+                    gambar: '/gambar/nduk1.jpg',
+                },
+                {
+                    gambar: '/gambar/nduk2.jpg',
+                },
+                {
+                    gambar: '/gambar/nduk3.jpg',
+                },
+            ],
+            isMobile: true,
+            currentSlide: 0,
+        }
+    },
+    watch: {
+        currentSlide(newIndex) {
+            // Periksa jika mencapai akhir dari slide
+            if (newIndex === this.images.length) {
+                this.currentSlide = 0; // Kembali ke slide pertama
+            }
+        }
+    },
+    methods: {
+        startAutoplay() {
+            setInterval(() => {
+                this.currentSlide++;
+            }, this.config.autoplay);
+        },
+    },
+    checkScreenSize() {
+        this.isMobile = window.innerWidth <= 480;
+    },
+    mounted() {
+        this.startAutoplay();
+        this.checkScreenSize();
+        window.addEventListener('resize', this.checkScreenSize);
+    }, beforeDestroy() {
+        window.removeEventListener('resize', this.checkScreenSize);
+    },
+}
+</script>
+
 <template>
     <div class="container">
+        <div class="isMobile" v-if="isMobile">
+            <Carousel v-bind="config" :modelValue="currentSlide">
+                <Slide v-for="(item, image) in images" :key="image">
+                    <img :src="item.gambar" alt="">
+                </Slide>
+                <template #addons>
+                    <Navigation />
+                    <Pagination />
+                </template>
+            </Carousel>
+        </div>
         <div class="leftM">
             <div class="content">
                 <h1>Juara 3</h1>
@@ -15,11 +89,20 @@
                 </router-link>
             </div>
         </div>
-        <div class="rightM">
-            <img src="" alt="">
+        <div class="rightM" v-if="!isMobile">
+            <Carousel v-bind="config" :modelValue="currentSlide">
+                <Slide v-for="(item, image) in images" :key="image">
+                    <img :src="item.gambar" alt="">
+                </Slide>
+                <template #addons>
+                    <Navigation />
+                    <Pagination />
+                </template>
+            </Carousel>
         </div>
     </div>
 </template>
+
 
 <style scoped>
 .container {
@@ -29,6 +112,13 @@
     align-items: center;
     margin: 0 50px;
 }
+
+.rightM {
+    max-width: 100%;
+    overflow: hidden;
+    /* Sembunyikan overflow */
+}
+
 .porto-btn {
     width: 150px;
     height: 30px;
@@ -87,8 +177,15 @@
     }
 
     .content p {
-        font-size: 150x;
-        margin: 20px 0;
+        display: none;
+    }
+
+    img {
+        width: 150px;
+        height: 200px;
+    }
+    .isMobile {
+        margin: 20px 0 0 0;
     }
 }
 </style>
